4138ddf03f2de5da24b5b84bfe2aaac8018135db
khj68 <maga40@naver.com>
2020-08-28 22:47:19 +0900

    Solve leetcode 451 oneliner

--- String/451_h.py
+++ String/451_h.py
@@ -11,3 +11,9 @@ class Solution:
             res += c*cnt
         
         return res
+
+
+##########One liner############
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        return ''.join([cnt*c for (cnt, c) in Counter(s).most_common()])
