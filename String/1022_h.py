3793f85737d857e16d08fa08786d94618cb25fcd
khj68 <maga40@naver.com>
2020-08-31 22:03:34 +0900

    Solve leetcode 1022 another method

--- String/1022_h.py
+++ String/1022_h.py
@@ -20,3 +20,13 @@ class Solution:
             if node.right : dfs(node.right, path + [node.val])
         dfs(root, [])
         return ans
+
+
+#########################
+
+class Solution:
+    def sumRootToLeaf(self, root, val=0):
+        if not root: return 0
+        val = val * 2 + root.val
+        if root.left == root.right: return val
+        return self.sumRootToLeaf(root.left, val) + self.sumRootToLeaf(root.right, val)
