75b98d0f8f1d639524917032d9d168adb482f7e5
khj68 <maga40@naver.com>
2020-09-02 14:35:30 +0900

    Solve leetcode 337

--- Dfs/39_h.py
+++ Dfs/39_h.py
@@ -14,3 +14,20 @@ class Solution:
                     dfs(remain - item, stack+[item])
         dfs(target, [])
         return res
+
+##########################
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        candidates.sort()
+        def dfs(k, path): 
+            # print(path)
+            if sum(path) == target: 
+                res.append(path)
+                return
+            if sum(path) > target : return
+            for i in range(k, len(candidates)):
+                dfs(i, path + [candidates[i]])
+        dfs(0, [])
+        return res
