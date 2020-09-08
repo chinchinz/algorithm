b21ae638fb47513f2a41d639e55105eb834fa799
khj68 <maga40@naver.com>
2020-09-08 11:22:00 +0900

    Solve programmers sort file

--- String/sortFile_h.py
+++ String/sortFile_h.py
@@ -1,12 +1,13 @@
 import re
 
 def solution(files):
-    ans = [[None]*3 for _ in range(len(files))]
+    ans = [0 for _ in range(len(files))]
     for i, file in enumerate(files):
-        ans[i][1] = re.findall('\d+', file)[0]
-        print(re.split('\d+', file))
-        ans[i][0], ans[i][2] = re.split('\d+', file)
+        # ans[i][1] = re.findall('\d+', file)[0]
+        print(re.split(r'(\d+)', file))
+        ans[i] = re.split(r'(\d+)', file)
     
+    # print(ans)
     ans.sort(key=lambda x: (x[0].lower(), int(x[1])))
     return [''.join(x) for x in ans]
 
