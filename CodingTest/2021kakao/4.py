7cc439a81123fb4f340ec652b198c1c8ebbe396b
KHJ <maga40@naver.com>
2020-09-12 15:36:35 +0900

    Solve three probelms perfectly

--- CodingTest/2021kakao/4.py
+++ CodingTest/2021kakao/4.py
@@ -1,9 +1,9 @@
-def solution():
+def solution(n, s, a, b, fares):
   ans = 0
 
   return ans
 
 
-print(solution())
+print(solution(6, 4, 6, 2, [[4, 1, 10], [3, 5, 24], [5, 6, 2], [3, 1, 41], [5, 1, 24], [4, 6, 50], [2, 4, 66], [2, 3, 22], [1, 6, 25]]))
 # print(solution())
 # print(solution())
