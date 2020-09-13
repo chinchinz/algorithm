520409b1806da7f284e264f692006ae2ae4b0352
KHJ <maga40@naver.com>
2020-09-13 10:21:20 +0900

    Solve line 2nd problem

--- CodingTest/2021line/1.py
+++ CodingTest/2021line/1.py
@@ -1,9 +1,9 @@
-def solution():
+def solution(boxes):
   ans = 0
 
   return ans
 
 
-print(solution())
+print(solution([[1, 2], [2, 1], [3, 3], [4, 5], [5, 6], [7, 8]]))
 # print(solution())
 # print(solution())
